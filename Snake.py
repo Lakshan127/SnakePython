b6981563a342faae9d9d346bcf9501626e5751c7
Lakshan127 <m.lakshan@icloud.com>
2025-03-18 12:12:29 +0100

    first push

---
+++ Snake.py
@@ -0,0 +1,57 @@
+import tkinter
+import random
+
+ROW = 25
+COL = 25
+TILE_SIZE = 25
+
+FENSTER_BREITE = COL * TILE_SIZE
+FENSTER_HOHE = ROW * TILE_SIZE
+
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+# SpielFenster
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False,False)
+
+canvas = tkinter.Canvas(window, bg='gray', width=FENSTER_BREITE,height=FENSTER_HOHE,borderwidth=0, highlightthickness=0)
+canvas.pack()
+window.update()
+
+#center the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+window.geometry (f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+
+#Spiel Initialisirung
+snake = Tile(5*TILE_SIZE,5*TILE_SIZE)#Kopf der Schlange
+food = Tile(10*TILE_SIZE,10*TILE_SIZE)
+velocityX = 0
+velocityY = 0
+
+def draw():
+    global snake
+
+    #Schlange 
+    canvas.create_rectangle(snake.x,snake.y,snake.x+TILE_SIZE,snake.y+TILE_SIZE,fill='lime green',outline='')
+    #Essen
+    canvas.create_rectangle(food.x,food.y,food.x+TILE_SIZE,food.y+TILE_SIZE,fill='red',outline='black')
+    window.after(100,draw)
+
+draw()
+
+
+window.bind("<KeyRelease>",)
+window.mainloop()
